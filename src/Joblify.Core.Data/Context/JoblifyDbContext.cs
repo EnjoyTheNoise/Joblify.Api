c7971f3d4abbb4dd106703382125435a1e4183cb
blc132 <pawelkobrynski97@gmail.com>
2019-04-10 15:28:30 +0200

    Added empty constructor and overrided OnConfiguring method (needed for migration)

--- src/Joblify.Core.Data/Context/JoblifyDbContext.cs
+++ src/Joblify.Core.Data/Context/JoblifyDbContext.cs
@@ -5,11 +5,23 @@ namespace Joblify.Core.Data.Context
 {
     public class JoblifyDbContext : DbContext
     {
+        public JoblifyDbContext()
+        {
+        }
+
         public JoblifyDbContext(DbContextOptions<JoblifyDbContext> options) : base(options) { }
 
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<ExternalProvider> ExternalProviders { get; set; }
         public virtual DbSet<Role> Roles { get; set; }
         public virtual DbSet<File> Files { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Joblify;Trusted_Connection=True;");
+            }
+        }
     }
 }
