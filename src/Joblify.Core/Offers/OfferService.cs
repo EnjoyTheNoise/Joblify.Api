f27c8c9d98bca4dc16da277e4e60d7fe718bb2b2
Astenna <marekkinga97@gmail.com>
2019-04-23 22:05:05 +0200

    added dtos and service for Offers

---
+++ src/Joblify.Core/Offers/OfferService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Joblify.Core.Data.Models;
+using Joblify.Core.Data.UnitOfWork;
+using System.Threading.Tasks;
+
+namespace Joblify.Core.Offers
+{
+    public class OfferService : IOfferService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public OfferService(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<OfferDto> AddOfferAsync(OfferDto offerDto)
+        {
+            var offer = _mapper.Map<Offer>(offerDto);
+            offer.IsDeleted = false;
+
+            await _unitOfWork.OfferRepository.AddAsync(offer);
+            await _unitOfWork.CommitAsync();
+
+            var created = _mapper.Map<OfferDto>(offer);
+
+            return created;
+        }
+    }
+}
